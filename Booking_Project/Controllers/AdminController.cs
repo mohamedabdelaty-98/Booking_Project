6c730c5030f289d87a7a336b11eba81d2f1a55dc
Mohamed Abdelaty <67396326+mohamedabdelaty-98@users.noreply.github.com>
2023-10-14 04:12:38 +0300

    Update AdminController.cs

--- Booking_Project/Controllers/AdminController.cs
+++ Booking_Project/Controllers/AdminController.cs
@@ -1,76 +1,14 @@
-﻿using Booking_Project.Models;
-using Booking_Project.Reposatory;
-using Booking_Project1.Models;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Booking_Project.Controllers
 {
-    //[Authorize(Roles ="Admin")]
+    [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
-        private readonly ICrudOperation<Room> RoomRepo;
-        private readonly ICrudOperation<Hotel> hotelrepo;
-
-        public AdminController(ICrudOperation<Room> RoomRepo,ICrudOperation<Hotel> hotelRepo)
-        {
-            this.RoomRepo = RoomRepo;
-            this.hotelrepo = hotelRepo;
-        }
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Rooms()
-        {
-            List<Room> RoomModel = RoomRepo.GetAll(h=>h.hotel);
-           
-            return View("room",RoomModel);
-        }
-        public IActionResult EditRoom(int id)
-        {
-            Room rs = RoomRepo.GetById(id);
-            return View("EditRoom", rs);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult EditRoom(Room rs, int id)
-        {
-            if (rs.room_number != null)
-            {
-                RoomRepo.update(rs);
-                RoomRepo.save();
-                return RedirectToAction("Rooms");
-            }
-            return View("EditRoom", rs);
-        }
-        public IActionResult DeleteRoom(int id)
-        {
-            RoomRepo.Delete(id);
-            RoomRepo.save();
-            return RedirectToAction("Rooms");
-        }
-        public IActionResult NewRoom()
-        {
-            List<Room> RoomModel = RoomRepo.GetAll(h => h.hotel);
-
-            
-            return View("new", RoomModel);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult NewRoom(Room room)
-        {
-            if (ModelState.IsValid)
-            {
-                RoomRepo.insert(room);
-                RoomRepo.save();
-                return RedirectToAction("Rooms");
-            }
-            List<Room> RoomModel = RoomRepo.GetAll(h => h.hotel);
-
-            return View("new", room);
         }
 
     }
