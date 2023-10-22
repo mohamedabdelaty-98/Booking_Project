c75e82e817dc4b64d75bad9d581a0eec003809fa
mohamedabdelaty-98 <abdelatym775@gmail>
2023-10-22 04:29:44 +0300

    update in admin  dashboard

--- Booking_Project/wwwroot/js/Hotel_ajax.js
+++ Booking_Project/wwwroot/js/Hotel_ajax.js
@@ -2,10 +2,9 @@
 let insertHotel = document.getElementById("insert_s");
 let addimage = document.getElementById("add_hotel_image");
 
-
-
 function DisplayHotels() {
     room_disp.classList.add("display_visable")
+    aminietes_disp.classList.add("display_visable");
     hotel_disp.classList.remove("display_visable");
     $.ajax(
         {
@@ -19,11 +18,11 @@ function DisplayHotels() {
     );
 }
 
-
 //section room
 var room_disp = document.getElementById("rooms");
 function DisplayRooms() {
     hotel_disp.classList.add("display_visable")
+    aminietes_disp.classList.add("display_visable");
     room_disp.classList.remove("display_visable");
     
     console.log("dddd");
@@ -33,6 +32,25 @@ function DisplayRooms() {
             success: function (result) {
                 console.log(result);
                 $("#rooms").html(result);
+            }
+        }
+
+    );
+}
+
+var aminietes_disp = document.getElementById("Amenties");
+function DisplayAmenties() {
+    hotel_disp.classList.add("display_visable")
+    room_disp.classList.add("display_visable");
+    aminietes_disp.classList.remove("display_visable");
+
+    console.log("dddd");
+    $.ajax(
+        {
+            url: "/Amenities/getall/",
+            success: function (result) {
+                console.log(result);
+                $("#Amenties").html(result);
             }
         }
 
